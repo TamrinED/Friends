4c3f4bdda47e187f36fc1c323f0b2117552c4140
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-05 15:52:12 -0600

    Update payment.php

--- payment.php
+++ payment.php
@@ -4,7 +4,7 @@
  <h1 style="text-align:center; font-family:Serif;  font-size:4rem">Payments</h1>
     <table style="background-color:#CFD8DC" class="table table-striped">
  
-<table class="table table-striped">
+<table style="background-color:#CFD8DC" class="table table-striped">
   <thead>
     <tr>
       <th>Payment ID</th>
