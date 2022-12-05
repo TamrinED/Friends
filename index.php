40fc0e5a2f02f7c31aa150b368b93de2a2e4803c
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-05 13:08:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php require_once("header.php"); ?>
 
-<body bgcolor="blue">
 
 <h1 style="text-align: center;">Hotel Reservations</h1>
 <h2 style="text-align: center;">Hello welcome to our website! On this website you can find infromation about our guests reservations, their payment type, and what room they receive.</h2>
