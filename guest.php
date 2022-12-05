76733a0e617c11e390b864b6a66db1e351461680
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-05 13:30:17 -0600

    Update guest.php

--- guest.php
+++ guest.php
@@ -1,6 +1,6 @@
 <?php require_once("header.php"); ?>
 
-<h1 style="text-align:center; font-family:Serif;">Guests</h1>
+<h1 style="text-align:center; font-family:Serif;  font-size:4rem">Guests</h1>
 <table style="background-color:#CFD8DC" class="table table-striped">
   <thead>
     <tr>
