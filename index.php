40468c17b2f10020a9ecbe168d6f4f877b3b1233
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-12-04 19:26:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,31 @@
 <h1 style="text-align: center;">Hotel Reservations</h1>
 <h2 style="text-align: center;">Hello welcome to our website! On this website you can find infromation about our guests reservations, their payment type, and what room they receive.</h2>
 
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<body>
+
+<div id="myPlot" style="width:100%;max-width:700px"></div>
+
+<script>
+var xArray = [50,60,70,80,90,100,110,120,130,140,150];
+var yArray = [7,8,8,9,9,9,10,11,14,14,15];
+
+// Define Data
+var data = [{
+  x: xArray,
+  y: yArray,
+  mode:"lines"
+}];
+
+// Define Layout
+var layout = {
+  xaxis: {range: [40, 160], title: "Square Meters"},
+  yaxis: {range: [5, 16], title: "Price in Millions"},  
+  title: "House Prices vs. Size"
+};
+
+// Display using Plotly
+Plotly.newPlot("myPlot", data, layout);
+</script>
+
 <?php require_once("footer.php"); ?>
