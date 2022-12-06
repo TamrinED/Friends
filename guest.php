3a2c8fccd80455977d05f87c8bc7bad6e8c4fc03
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-06 17:56:44 -0600

    Update guest.php

--- guest.php
+++ guest.php
@@ -18,7 +18,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into Guest (Name, Email, Phone) values (?)";
+      $sqlAdd = "insert into Guest (Name, Email, Phone) values (?, ?, ?)";
       $stmtAdd = $conn->prepare($sqlAdd);
       $stmtAdd->bind_param("s", $_POST['iName'], $_POST['iEmail'], $_POST['iPhone']);
       $stmtAdd->execute();
