1d74c9ac334db472e0f0641b8b8067dda429b651
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-12-06 16:46:40 -0600

    Update guest.php

--- guest.php
+++ guest.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       echo '<div class="alert alert-success" role="alert">New guest added.</div>';
       break;
     case 'Edit':
-      $sqlEdit = "update Guest set Name=?, Email=? where GuestID=?";
+      $sqlEdit = "update Guest set Name= 'iName', Email='iEmail' where GuestID=?";
       $stmtEdit = $conn->prepare($sqlEdit);
       $stmtEdit->bind_param("si", $_POST['iName'], $_POST['iEmail'], $_POST['iid']);
       $stmtEdit->execute();
