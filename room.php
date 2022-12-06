18d9c79998b8875dadf8cbe02d06eaa8de27fdde
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-12-06 17:33:53 -0600

    Update room.php

--- room.php
+++ room.php
@@ -20,7 +20,7 @@ var data = [{
 var layout = {
   xaxis: {range: [1, 12], title: "Months"},
   yaxis: {range: [10, 200], title: "Rooms Booked"},  
-  style.backgroundColor= "black";
+  style.backgroundColor= "black",
   title: "The Number of Rooms Booked by Guests per Month in 2022"
   
 };
