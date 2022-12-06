518648bdfd1d141e7244d49ff4d22bb13264b3d8
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-12-06 17:07:53 -0600

    Create guest.php

--- guest.php
+++ guest.php
@@ -18,9 +18,9 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into Guest (Name, Email, Phone) value (?,?,?)";
+      $sqlAdd = "insert into Guest (Name) value (?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['iName'], $_POST['iEmail'], $_POST['iPhone']);
+      $stmtAdd->bind_param("s", $_POST['iName']);
       $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">New guest added.</div>';
       break;
@@ -31,8 +31,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $sqlEdit = "update Guest set Email=? where GuestID=?";
       $stmtEdit = $conn->prepare($sqlEdit);
       $stmtEdit->bind_param("si", $_POST['iEmail'], $_POST['iid']);
+      $sqlEdit = "update Guest set Phone=? where GuestID=?";
+      $stmtEdit = $conn->prepare($sqlEdit);
+      $stmtEdit->bind_param("si", $_POST['iPhone'], $_POST['iid']);
       $stmtEdit->execute();
-      echo '<div class="alert alert-success" role="alert">Guest edited.</div>';
+      echo '<div class="alert alert-success" role="alert">Guest information edited.</div>';
       break;
     case 'Delete':
       $sqlDelete = "delete from Guest where GuestID=?";
@@ -89,6 +92,8 @@ if ($result->num_rows > 0) {
                           <input type="text" class="form-control" id="editGuest<?=$row["GuestID"]?>Name" aria-describedby="editGuest<?=$row["GuestID"]?>Help" name="iName" value="<?=$row['Name']?>">
                            <label for="editGuest<?=$row["GuestID"]?>Email" class="form-label">Guest Email</label>
                           <input type="text" class="form-control" id="editGuest<?=$row["GuestID"]?>Email" aria-describedby="editGuest<?=$row["GuestID"]?>Help" name="iEmail" value="<?=$row['Email']?>">
+                          <label for="editGuest<?=$row["GuestID"]?>Phone" class="form-label">Guest Phone Number</label>
+                          <input type="text" class="form-control" id="editGuest<?=$row["GuestID"]?>Phone" aria-describedby="editGuest<?=$row["GuestID"]?>Help" name="iPhone" value="<?=$row['Phone']?>">
                           <div id="editGuest<?=$row["GuestID"]?>Help" class="form-text">Enter the guest's information.</div>
                         </div>
                         <input type="hidden" name="iid" value="<?=$row['GuestID']?>">
@@ -139,11 +144,7 @@ $conn->close();
                 <div class="mb-3">
                   <label for="Name" class="form-label">Guest Name</label>
                   <input type="text" class="form-control" id="Name" aria-describedby="nameHelp" name="iName">
-                  <label for="Name" class="form-label">Guest Email</label>
-                  <input type="text" class="form-control" id="Email" aria-describedby="emailHelp" name="iEmail">
-                  <label for="Name" class="form-label">Guest Phone Number</label>
-                  <input type="text" class="form-control" id="Phone" aria-describedby="emailHelp" name="iPhone">
-                  <div id="nameHelp" class="form-text">Enter the Guest's information.</div>
+                  <div id="nameHelp" class="form-text">Enter the Guest's name.</div>
                 </div>
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
