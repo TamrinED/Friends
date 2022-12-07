cd62bdb15f2eea97d6fb602ca2dfd5043498235b
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-07 12:27:53 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php require_once("header.php"); ?>
-<body style="background-color:#B2DFDB">
 
 <h1 style="text-align:center; font-family:Serif;  font-size:4rem"><span id="title">Utopia Hotel Employee Website</span></h1>
 
@@ -21,7 +20,6 @@
   </div>
     
   </div>
-</div>
 
 
 <?php require_once("footer.php"); ?>
