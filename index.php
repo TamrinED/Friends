61fee8ddfd70143e62bf94da8e1a819e52a937b8
RebeccaWatley <89473987+RebeccaWatley@users.noreply.github.com>
2022-12-06 19:24:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 
 <h1 style="text-align:center; font-family:Serif;  font-size:4rem"><span id="title">Utopia Hotel Employee Website</span></h1>
 
-  
+
+<div class="container">
+  <img src="https://th.bing.com/th/id/OIP.GEA7TEUSNiV4dmfBaDRKWwHaLH?pid=ImgDet&rs=1" alt="Avatar" class="image" style="width:100%">
+  <div class="middle">
+    <div class="text">Kyle Robb</div>
+  </div>
+</div>
+ 
 
 <?php require_once("footer.php"); ?>
