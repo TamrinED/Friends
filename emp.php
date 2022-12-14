2750014b7699683e41277523b2560041fa7cc878
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-13 22:36:27 -0600

    Update emp.php

--- emp.php
+++ emp.php
@@ -2,7 +2,7 @@
 
 <h1 style="text-align:center; font-family:Serif;  font-size:4rem"><span id="title">Utopia Hotel Employee Website</span></h1>
 
-<h2><span id="welcomemp"> Welcome </span><?php echo $_POST["name"]; ?><br>
+<h2> Welcome<?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?>
 </h2>
   
