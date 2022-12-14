0534682261dda31103984e1b93dc74065dfdd423
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-13 20:26:38 -0600

    Update guest.php

--- guest.php
+++ guest.php
@@ -18,9 +18,9 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
-      $sqlAdd = "insert into Guest (GuestID, Name, Email, Phone) values ('?', '?', '?', '?')";
+      $sqlAdd = "insert into Guest (Name, Email, Phone) values ('?', '?', '?')";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $_POST['iName'], $_POST['iEmail'], $_POST['iPhone']);
+      $stmtAdd->bind_param($_POST['iName'], $_POST['iEmail'], $_POST['iPhone']);
       $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">New guest information added.</div>';
       break;
