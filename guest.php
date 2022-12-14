dd37def8754630dac50a2b37cd5997cd71c71244
TamrinED <98431860+TamrinED@users.noreply.github.com>
2022-12-13 20:41:30 -0600

    Update guest.php

--- guest.php
+++ guest.php
@@ -15,16 +15,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-  $iName = $_POST['iName'];
-  $iEmail = $_POST['iEmail'];
-  $iPhone = $_POST['iPhone'];
   
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   switch ($_POST['saveType']) {
     case 'Add':
       $sqlAdd = "insert into Guest (Name, Email, Phone) values (?), (?), (?)";
       $stmtAdd = $conn->prepare($sqlAdd);
-      $stmtAdd->bind_param("s", $iName, $iEmail, $iPhone);
+      $stmtAdd->bind_param("s", $_POST['iName'], $_POST['iEmail'], $_POST['iPhone']);
       $stmtAdd->execute();
       echo '<div class="alert alert-success" role="alert">New guest information added.</div>';
       break;
